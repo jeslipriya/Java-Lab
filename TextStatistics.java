003984608887f1407bc3d7b7c9ee28beb9e93913
Jesli <jeslipriya07@gmail.com>
2025-04-03 19:23:15 +0530

    Added TextStatistics.java and Demo.txt

---
+++ TextStatistics.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TextStatistics {
+    public static void main(String[] args) {
+        String filepath = "C:/Users/jesli/OneDrive/Desktop/Java-Lab/Demo.txt";
+        try(BufferedReader reader = new BufferedReader(new FileReader(filepath))){
+            String line;
+            int wordCount = 0;
+            int lineCount = 0;
+            int charCount = 0;
+
+            while((line = reader.readLine()) != null ){
+                lineCount++;
+                charCount += line.length();
+                wordCount += line.split("\\s+").length;
+            }
+
+            System.out.println("Character count = " + charCount);
+            System.out.println("Word count = " + wordCount);
+            System.out.println("Line count = " + lineCount);
+        }
+
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
