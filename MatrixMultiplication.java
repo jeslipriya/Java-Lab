b14e2c383215c6b4ba7a12a006b4443ec4897b6a
Jesli <jeslipriya07@gmail.com>
2025-04-03 20:26:38 +0530

    Updated with try-catch

--- MatrixMultiplication.java
+++ MatrixMultiplication.java
@@ -2,54 +2,58 @@ import java.util.Scanner;
 
 public class MatrixMultiplication {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        try (Scanner scan = new Scanner(System.in)){
 
-        System.out.println("Enter the number of rows of Matrix A");
-        int rowsA = scan.nextInt();
+            System.out.println("Enter the number of rows of Matrix A");
+            int rowsA = scan.nextInt();
 
-        System.out.println("Enter the number of columns of matrix A");
-        int colsA = scan.nextInt();
+            System.out.println("Enter the number of columns of matrix A");
+            int colsA = scan.nextInt();
 
-        System.out.println("Enter the number of rows of Matrix B");
-        int rowsB = scan.nextInt();
+            System.out.println("Enter the number of rows of Matrix B");
+            int rowsB = scan.nextInt();
         
-        System.out.println("Enter the number of columns of Matrix B");
-        int colsB = scan.nextInt();
+            System.out.println("Enter the number of columns of Matrix B");
+            int colsB = scan.nextInt();
 
-        int[][] a = new int [rowsA] [colsA];
-        int[][] b = new int [rowsB] [colsB];
-        int[][] c = new int [rowsA] [colsB];
+            int[][] a = new int [rowsA] [colsA];
+            int[][] b = new int [rowsB] [colsB];
+            int[][] c = new int [rowsA] [colsB];
 
-        System.out.println("Enter the elements of matrix A");
-        for(int i=0; i<rowsA; i++){
-            for(int j=0; j<colsA; j++){
-                a[i][j] = scan.nextInt();
+            System.out.println("Enter the elements of matrix A");
+            for(int i=0; i<rowsA; i++){
+                for(int j=0; j<colsA; j++){
+                    a[i][j] = scan.nextInt();
+                }       
             }
-        }
 
-        System.out.println("Enter the elements of matrix B");
-        for(int i=0; i<rowsB; i++){
-            for(int j=0; j<colsB; j++){
-                b[i][j] = scan.nextInt();
-            }
-        }
-
-        for(int i=0; i<rowsA; i++){
-            for(int j=0; j<colsB; j++){
-                for(int k=0; k<colsA; k++){
-                    c[i][j] = a[i][k]*b[k][j];
+            System.out.println("Enter the elements of matrix B");
+            for(int i=0; i<rowsB; i++){
+                for(int j=0; j<colsB; j++){
+                    b[i][j] = scan.nextInt();
                 }
             }
-        }
 
-        System.out.println("the matrix multiplication is ");
-        for(int i=0; i<rowsA; i++){
-            for(int j=0; j<colsB; j++){
-                System.out.print(c[i][j] + " ");
+            for(int i=0; i<rowsA; i++){
+                for(int j=0; j<colsB; j++){
+                    for(int k=0; k<colsA; k++){
+                        c[i][j] = a[i][k]*b[k][j];
+                    }
+                }
             }
-            System.out.println();
+
+            System.out.println("the matrix multiplication is ");
+            for(int i=0; i<rowsA; i++){
+                for(int j=0; j<colsB; j++){
+                    System.out.print(c[i][j] + " ");
+                }
+                System.out.println();
+            }
+            
         }
-        
-        scan.close();
+    
+        catch(Exception e){
+            System.out.println("Please enter Numbers as input");
+        }   
     }
 }
--- RandomNumberGenerator.java
+++ RandomNumberGenerator.java
@@ -4,35 +4,42 @@ import java.util.Scanner;
 public class RandomNumberGenerator {
     public static void main(String[] args) {
         
-        Scanner scan = new Scanner(System.in);
-        Random random = new Random();
+        try (Scanner scan = new Scanner(System.in)){
+            Random random = new Random();
         
-        System.out.print("Enter the lower limit: ");
-        int lowerLimit = scan.nextInt();
+            System.out.print("Enter the lower limit: ");
+            int lowerLimit = scan.nextInt();
     
-        System.out.print("Enter the upper limit: ");
-        int upperLimit = scan.nextInt();
+            System.out.print("Enter the upper limit: ");
+            int upperLimit = scan.nextInt();
     
-        int randomNumber = random.nextInt(upperLimit-lowerLimit+1) + lowerLimit;
-        System.out.println("Generated random number is " + randomNumber);
+            int randomNumber = random.nextInt(upperLimit-lowerLimit+1) + lowerLimit;
+            System.out.println("Generated random number is " + randomNumber);
         
-        if(randomNumber < 0) {
-            System.out.println("The number is below Zero");
-        }
+            if(randomNumber < 0) {
+                System.out.println("The number is below Zero");
+            }
 
-        else if (randomNumber> 0 && randomNumber <= 10) {   
-            System.out.println("The number is between 0 and 10");
-        }
-        else if (randomNumber> 10 && randomNumber <= 20) {   
-            System.out.println("The number is between 10 and 20");
-        }
-        else if (randomNumber> 20 && randomNumber <= 50) {   
-            System.out.println("The number is between 20 and 50");
-        }
-        else {   
-            System.out.println("The number is above 50");
-        }
+            else if (randomNumber> 0 && randomNumber <= 10) {   
+                System.out.println("The number is between 0 and 10");
+            }
+            
+            else if (randomNumber> 10 && randomNumber <= 20) {   
+                System.out.println("The number is between 10 and 20");
+            }
 
-        scan.close();
+            else if (randomNumber> 20 && randomNumber <= 50) {   
+                System.out.println("The number is between 20 and 50");
+            }
+
+            else {   
+                System.out.println("The number is above 50");
+            }
+
+        }
+    
+        catch (Exception e) {
+            System.err.println("Please enter Number only!");
+        }
     }
 }
