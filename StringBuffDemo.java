c43ebc09ca6d18c7b242dfc285aa51ec7af775cc
Jesli <jeslipriya07@gmail.com>
2025-04-06 16:11:42 +0530

    Added StringBuffDemo.java

---
+++ StringBuffDemo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;                                                                       //length
+
+public class StringBuffDemo {                                                                     //reverse            
+    public static void main(String[] args) {                                                    //delete
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a String: ");
+        StringBuffer sb = new StringBuffer(scan.nextLine());
+        
+        System.out.println("The length of the string is " + sb.length());
+        System.out.println("The reverse of the string is " + sb.reverse());
+        System.out.println("Enter the start index to delete ");
+        int start = scan.nextInt();
+
+        System.out.println("Enter the end index to delete ");
+        int end = scan.nextInt();
+
+        if (start>=0 && end<sb.length() && start<end) {
+            sb.delete(start,end);
+            System.out.println("The string after deleting is " + sb);
+        }
+
+        else{System.out.println("Invalid indices");}
+
+        scan.close();
+    }
+}
