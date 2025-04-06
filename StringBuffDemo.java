6b5d7b7ec8ec460b041987b269d963e001d2f1dc
Jesli <jeslipriya07@gmail.com>
2025-04-06 17:08:12 +0530

    Updated StringBuffDemo.java

--- StringBuffDemo.java
+++ StringBuffDemo.java
@@ -2,26 +2,30 @@ import java.util.Scanner;                                                       
 
 public class StringBuffDemo {                                                                     //reverse            
     public static void main(String[] args) {                                                    //delete
-        Scanner scan = new Scanner(System.in);
+        try (Scanner scan = new Scanner(System.in)){
 
-        System.out.println("Enter a String: ");
-        StringBuffer sb = new StringBuffer(scan.nextLine());
+            System.out.println("Enter a String: ");
+            StringBuffer sb = new StringBuffer(scan.nextLine());
         
-        System.out.println("The length of the string is " + sb.length());
-        System.out.println("The reverse of the string is " + sb.reverse());
-        System.out.println("Enter the start index to delete ");
-        int start = scan.nextInt();
+            System.out.println("The length of the string is " + sb.length());
+            System.out.println("The reverse of the string is " + sb.reverse());
+            System.out.println("Enter the start index to delete ");
+            int start = scan.nextInt();
 
-        System.out.println("Enter the end index to delete ");
-        int end = scan.nextInt();
+            System.out.println("Enter the end index to delete ");
+            int end = scan.nextInt();
 
-        if (start>=0 && end<sb.length() && start<end) {
-            sb.delete(start,end);
-            System.out.println("The string after deleting is " + sb);
+            if (start>=0 && end<sb.length() && start<end) {
+                sb.delete(start,end);
+                System.out.println("The string after deleting is " + sb);
+            }
+
+            else{System.out.println("Invalid indices");}
+
         }
 
-        else{System.out.println("Invalid indices");}
-
-        scan.close();
+        catch(Exception e){
+            System.err.println("Please enter Strings only!");
+        }
     }
 }
