ad11d31ae54acfbbc3ba62349f3d784c911319ab
Jesli <jeslipriya07@gmail.com>
2025-04-09 20:06:59 +0530

    Added TextFormatter.java

---
+++ TextFormatter.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.text.AttributeSet;
+public class TextFormatter extends JFrame {
+    private JTextField textField;
+    private JTextArea textArea;
+
+    public TextFormatter(){
+        setTitle("Simple Text Formatter");
+        setSize(400, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+
+        textField = new JTextField(15);
+        textField.setBounds(50, 30, 300, 30);
+        add(textField);
+
+        JButton b = new JButton("Format Button");
+        b.setBounds(130, 80, 130, 30);
+        add(b);
+        textArea = new JTextArea(5,20);
+        textArea.setEditable(false);
+        
+        JScrollPane scroll = new JScrollPane(textArea);
+        scroll.setBounds(50, 130, 300, 200);
+        add(scroll);
+
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e){
+                formatText();
+            }
+        });
+
+        setVisible(true);
+    }
+
+    public void formatText(){
+        String text = textField.getText();
+        textArea.setText(text);
+        textArea.setFont(new Font("Arial",Font.PLAIN,16));
+        textArea.setText(text.toUpperCase());
+    }
+    public static void main(String[] args) {
+        new TextFormatter();       
+    }
+}
